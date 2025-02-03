e1cb35df14a851270ef4822010676ebf4a47dbb6
Antoine Barthelemy <antoine@linux.com>
2025-02-03 17:29:18 +0100

    feat: simple lib folder

---
+++ lib/tf.go
@@ -0,0 +1,56 @@
+package hcl
+
+import (
+	"fmt"
+		"github.com/zclconf/go-cty/cty"
+	"io/ioutil"
+
+	"tfau/module"
+
+	"github.com/hashicorp/hcl/v2/hclparse"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+)
+
+
+func UpdateVersionInFile(filename string) error {
+	parser := hclparse.NewParser()
+	file, diags := parser.ParseHCLFile(filename)
+	if diags.HasErrors() {
+		return fmt.Errorf("failed to parse HCL file: %v", diags)
+	}
+
+	syntaxFile, ok := file.Body.(*hclsyntax.Body)
+	if !ok {
+		return fmt.Errorf("failed to cast HCL body to syntax body")
+	}
+
+	for _, block := range syntaxFile.Blocks {
+		if block.Type == "module" {
+			var sourceValue string
+			var versionAttr *hclsyntax.Attribute
+
+			for name, attr := range block.Body.Attributes {
+				if name == "source" {
+					sourceVal, _ := attr.Expr.Value(nil)
+					sourceValue = sourceVal.AsString()
+				}
+				if name == "version" {
+					versionAttr = attr
+				}
+			}
+
+			if sourceValue != "" && versionAttr != nil {
+				newVersion, err := module.GetLatestVersion(sourceValue)
+				if err == nil {
+					versionAttr.Expr = &hclsyntax.LiteralValueExpr{
+						Val: cty.StringVal(newVersion),
+					}
+				}
+			}
+		}
+	}
+
+	output := hclwrite.Format([]byte(filename))
+	return ioutil.WriteFile(filename, output, 0644)
+}
--- main.go
+++ main.go
@@ -2,59 +2,10 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 
 	"tfau/lib"
-
-	"github.com/hashicorp/hcl/v2/hclparse"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/zclconf/go-cty/cty"
 )
-
-
-func updateVersionInFile(filename string) error {
-	parser := hclparse.NewParser()
-	file, diags := parser.ParseHCLFile(filename)
-	if diags.HasErrors() {
-		return fmt.Errorf("failed to parse HCL file: %v", diags)
-	}
-
-	syntaxFile, ok := file.Body.(*hclsyntax.Body)
-	if !ok {
-		return fmt.Errorf("failed to cast HCL body to syntax body")
-	}
-
-	for _, block := range syntaxFile.Blocks {
-		if block.Type == "module" {
-			var sourceValue string
-			var versionAttr *hclsyntax.Attribute
-
-			for name, attr := range block.Body.Attributes {
-				if name == "source" {
-					sourceVal, _ := attr.Expr.Value(nil)
-					sourceValue = sourceVal.AsString()
-				}
-				if name == "version" {
-					versionAttr = attr
-				}
-			}
-
-			if sourceValue != "" && versionAttr != nil {
-				newVersion, err := module.GetLatestVersion(sourceValue)
-				if err == nil {
-					versionAttr.Expr = &hclsyntax.LiteralValueExpr{
-						Val: cty.StringVal(newVersion),
-					}
-				}
-			}
-		}
-	}
-
-	output := hclwrite.Format([]byte(filename))
-	return ioutil.WriteFile(filename, output, 0644)
-}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -63,7 +14,7 @@ func main() {
 	}
 
 	filename := os.Args[1]
-	if err := updateVersionInFile(filename); err != nil {
+	if err := hcl.UpdateVersionInFile(filename); err != nil {
 		fmt.Println("Error:", err)
 	}
 }
